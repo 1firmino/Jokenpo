325723cbed232f8f68aaaae696e1782696daec64
1firmino <43869055+1firmino@users.noreply.github.com>
2019-06-28 10:11:29 -0300

    Initial commit

---
+++ js/jogo.js
@@ -0,0 +1,74 @@
+//Variáveis Globais
+let jogadaMaquina 
+let jogadaHumano
+
+
+function jogadas(jogada){
+
+    // REINICIA O ATIBUTO OPACITY
+    inicial()
+
+    //CRIA A JOGADA DA MÁQUINA
+        jogadaMaquina = Math.floor(Math.random()*3) 
+    
+    //RESGATA A JOGADA DO HUMANO E TRANSFORMA NUM VALOR PARA COMPARAR COM A JOGADA DA MÁQUINA
+    if(jogada === "Pedra"){
+       jogadaHumano = 0
+    } else if(jogada === "Papel"){
+       jogadaHumano = 1
+    } else if(jogada === "Tesoura"){
+       jogadaHumano = 2
+    } 
+
+    let vitorias = 0
+
+    //TESTE DOS VALORES DA JOGADA DA MÁQUINA E DO HUMANO PARA DECIDIR O RESULTADO
+    if(jogadaHumano === jogadaMaquina){
+        document.getElementById("resultado").innerHTML = "Empatou!"
+    }else if((jogadaHumano === 0 && jogadaMaquina === 1) || (jogadaHumano === 1 && jogadaMaquina === 2) || (jogadaHumano === 2 && jogadaMaquina === 0)){
+     document.getElementById("resultado").innerHTML = "Você perdeu!"
+     
+    }else if((jogadaMaquina === 0 && jogadaHumano === 1) || (jogadaMaquina === 1 && jogadaHumano === 2) || (jogadaMaquina === 2 && jogadaHumano === 0)){
+     let resultadoVitoria = document.getElementById("resultado")
+     resultadoVitoria.innerHTML = "Você ganhou!"
+     document.getElementById("vitorias").innerHTML = for (vitorias = 0; resultadoVitoria === "Você ganhou!"; vitorias++){}
+    }
+   
+    console.log("jogada da maquina foi " + jogadaMaquina)
+    console.log("jogada do Humano foi " + jogadaHumano)
+   
+    console.log(vitorias)
+    realce()
+    
+}
+
+// Função para realçar as jogadas feitas
+function realce(){
+    let clicadoMaquina = document.getElementsByClassName("jogadas maquina")
+    clicadoMaquina[jogadaMaquina].style.opacity = "1"
+    
+    let clicadoHumano = document.getElementsByClassName("jogadas humano")
+    clicadoHumano[jogadaHumano].style.opacity = "1"
+  
+
+}
+
+// Função para voltar o realce ao estado inicial
+function inicial(){
+    let clicadoMaquina = document.getElementsByClassName("jogadas maquina")
+    let i;
+    for (i = 0; i < clicadoMaquina.length; i++) { 
+        clicadoMaquina[i].style.opacity = "0.5"
+    }
+    
+    
+    clicadoHumano = document.getElementsByClassName("jogadas humano")
+    let y;
+    for (y = 0; y < clicadoHumano.length; y++) { 
+        clicadoHumano[y].style.opacity = "0.5"
+    }
+
+}
+
+
+
